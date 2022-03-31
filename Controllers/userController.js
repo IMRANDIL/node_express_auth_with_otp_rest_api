8ed611377ef261cd3f246c9eeed5f7a50cabd990
IMRANDIL <aliimranadil2@gmail.com>
2022-03-31 09:55:43 +0530

    yh

---
+++ Controllers/userController.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+
+const _ = require('lodash');
+
+
+const axios = require('axios');
+
+const otpGenerator = require('otp-generator');
+
+
+const Otp = require('../Model/otpModel');
+
+const User = require('../Model/userModel')
--- Model/otpModel.js
+++ Model/otpModel.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose');
 
 
 otpSchema = mongoose.Schema({
-
+    number: {
+        type: String,
+        required: true
+    },
+    otp: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        index: { expires: 300 }
+    }
 
 
 }, { timestamps: true })
